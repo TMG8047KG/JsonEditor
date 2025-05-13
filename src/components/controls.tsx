0ff649effaea64a5f94c509190f7a14a09c5057e
Александър Г. Горанов <ag17085559@edu.mon.bg>
2025-05-13 11:30:38 +0300

    design work

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -117,31 +117,11 @@ function Controls({
         <button className={styles.button} onClick={onClearTable}>Clear Table</button>
         <button className={styles.button} onClick={saveFile}>Save File</button>
       </div>
-      
-      <div className={styles.togglesContainer}>
-        <div className={styles.toggleWrapper}>
-          <label className={styles.toggleLabel}>
-            <span>Key Editable:</span>
-            <div 
-              className={`${styles.toggle} ${isKeyEditable ? styles.toggleOn : styles.toggleOff}`}
-              onClick={handleToggleKeyEditable}
-            >
-              <div className={styles.toggleSlider}></div>
-            </div>
-          </label>
-        </div>
-        <div className={styles.toggleWrapper}>
-          <label className={styles.toggleLabel}>
-            <span>Value Editable:</span>
-            <div 
-              className={`${styles.toggle} ${isValueEditable ? styles.toggleOn : styles.toggleOff}`}
-              onClick={handleToggleValueEditable}
-            >
-              <div className={styles.toggleSlider}></div>
-            </div>
-          </label>
-        </div>
-      </div>
+      <label className={styles.toggleLabel} onClick={handleToggleKeyEditable}>
+        <svg className={isKeyEditable ? styles.toggled : styles.untoggled} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z"/>
+        </svg>
+      </label>
     </div>
   );
 }
